c48ecc94c2925bbf617dbe3dc1da2aebe273efba
Dmitriy <62928282+Tarasovdv@users.noreply.github.com>
2024-01-31 12:59:46 +0300

    TASK #0 xPath Methods, Functions, Descriptions (#5)
    * add Properties 'n' create WebDriver with Property
    
    * add Tests_NG
    
    * Внесены правки
    
    * Внесены правки
    
    * add xPath.md
    
    * add WebElements
    
    * add xpath.properties
    
    * add SteamChromeTest.java
    and
    SANDBOXES
    
    * correct SteamChromeTest.java
    and
    delete SANDBOXES
    
    * correct SteamChromeTest.java
    
    * correct SteamChromeTest.java
    
    ---------
    
    Co-authored-by: Tarasovdv <desant74268@yande.ru>

--- src/main/java/seleniumHW/Properties.java
+++ src/main/java/seleniumHW/Properties.java
@@ -4,37 +4,18 @@ import java.io.*;
 
 public class Properties {
 
-    public static InputStream readPropFile(String name_file) {
-        ClassLoader classLoader = Properties.class.getClassLoader();
-        return classLoader.getResourceAsStream(name_file);
+    public static InputStream readPropFile(String nameFile) {
+        return Properties.class.getClassLoader().getResourceAsStream(nameFile);
     }
 
-    public static String selectParams(InputStream file_props, String key) {
+    public static String selectParams(InputStream fileProps, String key) {
         java.util.Properties props = new java.util.Properties();
         try {
-            props.load(file_props);
+            props.load(fileProps);
         } catch (IOException e) {
             System.out.println("Файл properties отсутствует");
             throw new RuntimeException(e);
         }
         return props.getProperty(key);
     }
-
-
-//    public static void main(String[] args) throws IOException {
-//
-//        // Первый способ
-//        File file = new File("src/main/resources/chrome.properties");
-//        Properties props = new Properties();
-//        props.load(new FileInputStream(file));
-//        System.out.println("props.getProperty(\"webdriver.chrome.driver\") = " + props.getProperty("webdriver.chrome.driver"));
-//
-//        // Второй способ
-//        ClassLoader classLoader = ReadProperties.class.getClassLoader();
-//        InputStream inputStream = classLoader.getResourceAsStream("chrome.properties");
-//
-//        Properties props2 = new Properties();
-//        props2.load(inputStream);
-//        System.out.println("props2.getProperty(\"webdriver.chrome.driver\") = " + props2.getProperty("webdriver.chrome.driver"));
-//    }
 }
---
+++ src/test/java/seleniumHW/hw3/SteamChromeTest.java
@@ -0,0 +1,79 @@
+package seleniumHW.hw3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static seleniumHW.Properties.readPropFile;
+import static seleniumHW.Properties.selectParams;
+
+public class SteamChromeTest {
+    private static final String propFile = "chrome.properties";
+    private final String urlSteamGeneral = selectParams(readPropFile(propFile), "url.steam.general");
+    private static WebDriver driver = WebDriverManager.getInstance(selectParams(readPropFile(propFile), "browser.name")).create();
+
+    @BeforeClass
+    public void createDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(urlSteamGeneral);
+    }
+
+    @AfterClass
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test(description = "Тест на наличие элемента: Текстовый эл-т 'КАТЕГОРИИ', находящийся внизу главной страницы.")
+    public static void categoryDownTextIsDisplayed() {
+        WebElement categoryDownTextElement = driver.findElement(By.xpath("//div[@class='home_page_content content_hub_carousel_ctn']/div[@class='title']"));
+        Assert.assertTrue(categoryDownTextElement.isDisplayed(), "Текстовый эл-т 'КАТЕГОРИИ', находящийся внизу главной страницы - не найден");
+    }
+
+    @Test(description = "Тест на наличие элемента: Текстовый эл-т 'КАТЕГОРИИ', находящийся в левом меню главной страницы.")
+    public static void categoryLeftTextIsDisplayed() {
+        WebElement categoryLeftTextElement = driver.findElement(By.xpath("//div[@role='main']//div[@class='home_page_gutter_block']/div[@class='gutter_header pad' and text() = 'Категории']"));
+        Assert.assertTrue(categoryLeftTextElement.isDisplayed(), "Текстовый эл-т 'КАТЕГОРИИ', находящийся в левом меню главной страницы - не найден");
+    }
+
+    @Test(description = "Тест на наличие элемента: Выпадающий список по кнопке 'Категории'")
+    public static void categoryPullDownButtonIsDisplayed() {
+        WebElement categoryPullDownButtonElement = driver.findElement(By.xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']"));
+        Assert.assertTrue(categoryPullDownButtonElement.isDisplayed(), "Выпадающий список по кнопке 'Категории' - не найден");
+    }
+
+    @Test(description = "Тест на наличие элемента: Кнопка-ссылка 'Приключенческая игра'", dependsOnMethods = {"categoryPullDownButtonIsDisplayed"})
+    public static void adventureGameLinkButtonIsDisplayed() {
+        WebElement categoryPullDownButtonElement = driver.findElement(By.xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']"));
+        categoryPullDownButtonElement.click();
+        WebElement adventureGameLinkButtonElement = driver.findElement(By.xpath("//a[contains(text(),'Приключенческая игра')]"));
+        Assert.assertTrue(adventureGameLinkButtonElement.isDisplayed(), "Кнопка-ссылка 'Приключенческая игра' - не найдена");
+    }
+
+    @Test(description = "Тест на соответствие текста элемента: Кнопка-ссылка 'Приключенческая игра'", dependsOnMethods = {"categoryPullDownButtonIsDisplayed"})
+    public static void adventureGameLinkButtonGetText() {
+        WebElement categoryPullDownButtonElement = driver.findElement(By.xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']"));
+        categoryPullDownButtonElement.click();
+        WebElement adventureGameLinkButtonElement = driver.findElement(By.xpath("//a[contains(text(),'Приключенческая игра')]"));
+        Assert.assertEquals(adventureGameLinkButtonElement.getText(), "Приключенческая игра", "Текст кнопки 'Приключенческая игра' - не соответствует ожидаемому результату");
+    }
+
+    @Test
+    public static void checkingCorrectnessOfPriceAndCurrencyTest() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://store.steampowered.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, " + 2250 + ");");
+        WebElement categoriesButtonTopMenuDesktop = driver.findElement(By.xpath("//img[@alt = 'TEKKEN 8']/ancestor::a[contains(@class,'app_impression_tracked')]//div[@class = 'discount_final_price']"));
+        Assert.assertTrue(categoriesButtonTopMenuDesktop.isDisplayed(), "Игра по указанному XPath не найдена.");
+        String[] parts = categoriesButtonTopMenuDesktop.getText().split(" ");
+        Assert.assertEquals(parts[0], "4199", "Указана некорректная цена товара");
+        Assert.assertEquals(parts[1], "pуб.", "Указана некорректная валюта в стоимости");
+    }
+}
