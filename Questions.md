35fced5d4da5fed6bc39e6707c08d11a08e53054
Tarasovdv <desant74268@yande.ru>
2024-02-12 13:32:36 +0300

    added: SteamChromeTest.java (package hw5) Questions.md
    modify:
    xPath.md
    SteamChromeTest.java (package hw3)
    Timeouts.md

--- Questions.md
+++ Questions.md
@@ -25,6 +25,9 @@
     JavascriptExecutor js = (JavascriptExecutor) driver;  
     js.executeScript("window.open('https://button-one.ru/')");
 
+интерфейс
+
+
 ### get()
 
     driver.get("https://store.steampowered.com/");
@@ -43,3 +46,54 @@
     FileUtils.copyFile(scrFile, new File("C:\\Users\\HANNYA\\IdeaProjects\\BUTTON-ONE\\Selenium_hw1\\src\\main\\resources\\scrn.png"));
 
 
+6. статический импорт
+
+
+7. проверить цвет элемента
+взять атрибут нашего элемента или (css value) вытащить код цвета и сравнить с ожидаемым. Сделать проверку: ДЗ
+   withTheHighestRatingParameterButton.getCssValue("color") = rgba(255, 255, 255, 1)
+
+
+8. Клик
+
+9. Expected Conditions - примеры конкретных
+
+10. Виды ожидания
+
+
+11. как проскролить Actions and JS
+12. `release():`                                                      Отпускает текущее удерживание клавиши или мыши.
+13. * `moveToElement(WebElement target):`                               Перемещает указатель к центру указанного элемента.
+
+14. * `contextClick():`                                                Выполняет контекстное (правой кнопкой мыши) меню в текущей позиции указателя.
+15. как очистить строку clear
+16. как понять что наш лого в верхнем левом углу getLocation у элемента getSize, getPozition 
+driver.manage().window().getSize();
+17. как проверить скачался ли файл при выполнении теста? 
+по пути куда сохраняем, и с помощью теста мы проверяем методом Exist Files 
+путь прописываем в опшенсах при инит драйвера
+18. какие элементы Selenium считает невидимыми
+если у элемента или родителя установлены display-non и visibility...
+19. как проверить видимость эл-та
+20. оси xPath
+21. какой локатор лучше?
+22. в чем отличие xPath есть оси. можно двигаться по осям вверх и вниз 
+23. типы локаторов
+24. классы НЕпроверяемых исключений Exeptions повторить
+25. что такое Selenium?
+26. рассказать про сущности Selenium
+driver - управление браузером, весь тест завязан на драйвере
+element -  елементы на странице сайта
+locator - указатель на элемент . класс-By .   абстракция над локатором
+27. как долго действует Неявное ожидание по отношению к экземпляру класса
+драйвер. до тех пор пока не закроем драйвер
+28. переключить на другое окно swichTo().window(.....)  
+29. получить данные страницы HTML
+getPageSource
+getAttribute*
+JS
+30. установить директорию загрузки
+options experimental
+31. явные и неявные ожидания вместе можно ли 
+32. transient
+33. StaleElementReferenceException - ссылка на устаревший элемент
---
+++ src/test/java/seleniumHW/hw5/SteamChromeTest.java
@@ -0,0 +1,151 @@
+package seleniumHW.hw5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+import static org.testng.Assert.assertTrue;
+import static seleniumHW.Properties.readPropFile;
+import static seleniumHW.Properties.selectParams;
+
+public class SteamChromeTest {
+    private static final String propFile = "chrome.properties";
+    private final String urlSteamGeneral = selectParams(readPropFile(propFile), "url.steam.general");
+    private static WebDriver driver = WebDriverManager.getInstance(selectParams(readPropFile(propFile), "browser.name")).create();
+
+    private static WebDriverWait wait;
+//    private static final JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    @BeforeClass
+    public void createDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        driver.get(urlSteamGeneral);
+    }
+
+    @AfterClass
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test(description = "Тест на наличие элемента: Текстовый эл-т 'КАТЕГОРИИ', находящийся внизу главной страницы.")
+    public static void categoryDownTextIsDisplayed() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        assertTrue(wait.until(visibilityOfElementLocated(By
+                                .xpath("//div[@class='home_page_content content_hub_carousel_ctn']/div[@class='title']")))
+                        .isDisplayed(),
+                "Текстовый эл-т 'КАТЕГОРИИ', находящийся внизу главной страницы - не найден");
+    }
+
+    @Test(description = "Тест на наличие элемента: Текстовый эл-т 'КАТЕГОРИИ', находящийся в левом меню главной страницы.")
+    public static void categoryLeftTextIsDisplayed() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        assertTrue(wait.until(visibilityOfElementLocated(By
+                                .xpath("//div[@role='main']//div[@class='home_page_gutter_block']/div[@class='gutter_header pad' and text() = 'Категории']")))
+                        .isDisplayed(),
+                "Текстовый эл-т 'КАТЕГОРИИ', находящийся в левом меню главной страницы - не найден");
+    }
+
+    @Test(description = "Тест на наличие элемента: Выпадающий список по кнопке 'Категории'")
+    public static void categoryPullDownButtonIsDisplayed() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        assertTrue(wait.until(visibilityOfElementLocated(By
+                        .xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']")))
+                .isDisplayed(), "Выпадающий список по кнопке 'Категории' - не найден");
+    }
+
+    @Test(description = "Тест на наличие элемента: Кнопка-ссылка 'Приключенческая игра'"
+            , dependsOnMethods = {"categoryPullDownButtonIsDisplayed"})
+    public static void adventureGameLinkButtonIsDisplayed() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement categoryPullDownButtonElement = wait.until(visibilityOfElementLocated(By
+                .xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']")));
+        categoryPullDownButtonElement.click();
+        assertTrue(wait.until(visibilityOfElementLocated
+                        (By.xpath("//a[contains(text(),'Приключенческая игра')]")))
+                .isDisplayed(), "Кнопка-ссылка 'Приключенческая игра' - не найдена");
+    }
+
+    @Test(description = "Тест на соответствие текста элемента: Кнопка-ссылка 'Приключенческая игра'"
+            , dependsOnMethods = {"categoryPullDownButtonIsDisplayed"})
+    public static void adventureGameLinkButtonGetText() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement categoryPullDownButtonElement = wait.until(visibilityOfElementLocated
+                (By.xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']")));
+        categoryPullDownButtonElement.click();
+        Assert.assertEquals(wait.until(visibilityOfElementLocated(By
+                                .xpath("//a[contains(text(),'Приключенческая игра')]")))
+                        .getText()
+                , "Приключенческая игра"
+                , "Текст кнопки 'Приключенческая игра' - не соответствует ожидаемому результату");
+    }
+
+    @Test
+    public static void checkingCorrectnessOfPriceAndCurrency() {
+        driver.get("https://store.steampowered.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, " + 2250 + ");");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement categoriesButtonTopMenuDesktop = wait.until(visibilityOfElementLocated(By
+                .xpath("//img[@alt = 'Симулятор Чушпана']/ancestor::a[contains(@class,'app_impression_tracked')]//div[@class = 'discount_final_price']")));
+        assertTrue(categoriesButtonTopMenuDesktop.isDisplayed(), "Игра по указанному XPath не найдена.");
+        String[] parts = categoriesButtonTopMenuDesktop.getText().split(" ");
+        Assert.assertEquals(parts[0], "48", "Указана некорректная цена товара");
+        Assert.assertEquals(parts[1], "pуб.", "Указана некорректная валюта в стоимости");
+    }
+
+    @Test(description = "Тест на соответствие корректного перехода на страницу ИГРЫ по пути:" +
+            " Главная страница/Категории/Кооперативы/С наивысшим рейтингом" +
+            "/Фильтры:Основные жанры-Казуальная игра/Игроки-кооператив"
+            , dependsOnMethods = {"categoryPullDownButtonIsDisplayed"})
+    public static void checkCorrectNameGame() {
+        driver.get("https://store.steampowered.com/");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement categoryPullDownButtonElement = wait.until(visibilityOfElementLocated(By
+                .xpath("//div//a[@class='pulldown_desktop' and text() = 'Категории']")));
+        categoryPullDownButtonElement.click();
+        WebElement cooperativesLinkButtonElement = wait.until(visibilityOfElementLocated(By
+                .xpath("//a[contains(text(),'Кооперативы')]")));
+        cooperativesLinkButtonElement.click();
+        scrollJS(1890);
+        WebElement filterGamesButton = wait.until(visibilityOfElementLocated(By
+                .xpath("//div[contains(text(),'Фильтры')]")));
+        filterGamesButton.click();
+        WebElement casualGameButton = wait.until(visibilityOfElementLocated(By
+                .xpath("//div[contains(text(),'Основные жанры')]/following::a[contains(text(),'Казуальная игра')]")));
+        casualGameButton.click();
+        scrollJS(300);
+        WebElement gamersButton = wait.until(visibilityOfElementLocated(By
+                .xpath("//div[contains(text(),'Игроки')]")));
+        gamersButton.click();
+        WebElement cooperativesButton = wait.until(visibilityOfElementLocated(By
+                .xpath("//div[contains(text(),'Игроки')]/following::a[contains(text(),'Кооператив')]")));
+        cooperativesButton.click();
+        scrollJS(-300);
+        driver.get(wait.until(visibilityOfElementLocated(By
+                        .xpath("//div[@class='facetedbrowse_FacetedBrowseItems_NO-IP']//a/div[contains(text(),\"Garry's Mod\")]/..")))
+                .getAttribute("href"));
+        Assert.assertEquals(wait.until(visibilityOfElementLocated
+                                (By.xpath("//div[@id = 'appHubAppName']")))
+                        .getText()
+                , "Garry's Mod"
+                , "Произведен некорректный выбор игры");
+    }
+
+    private static void scrollJS(int scroll) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, " + scroll + ");");
+    }
+}
