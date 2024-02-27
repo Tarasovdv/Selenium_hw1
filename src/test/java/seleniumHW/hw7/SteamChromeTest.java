499664f4b31147152eccd4efac37b1587dadbdb2
Tarasovdv <desant74268@yande.ru>
2024-02-27 20:09:12 +0300

    add Selenid modify

--- src/test/java/seleniumHW/hw7/SteamChromeTest.java
+++ src/test/java/seleniumHW/hw7/SteamChromeTest.java
@@ -18,31 +18,34 @@ import static hw7.utils.Properties.selectParams;
 public class SteamChromeTest {
     private static final String PROP_FILE = "chrome.properties";
     private static final String URL_STEAM_GENERAL = selectParams(readPropFile(PROP_FILE), "url.steam.general");
-    private static final CoopPage COOP_PAGE = new CoopPage();
-    private static final SteamMainPage MAIN_PAGE = new SteamMainPage();
-    private static final GarrysModPage GARRYS_MOD_PAGE = new GarrysModPage();
 
     @AfterTest
     public void quitDriver() {
         closeWebDriver();
     }
 
-    @Test(description = "Тестирование корректного перехода на страницу ИГРЫ по пути:\" " + "            \" Главная страница/Категории/Кооперативы/С наивысшим рейтингом\" +\n" + "            \"/Фильтры:Основные жанры-Казуальная игра/Игроки-кооператив")
+    @Test(description = "Тестирование корректного перехода на страницу ИГРЫ по пути:\" " +
+            "            \" Главная страница/Категории/Кооперативы/С наивысшим рейтингом\" +\n" +
+            "            \"/Фильтры:Основные жанры-Казуальная игра/Игроки-кооператив")
     public void checkingCorrectProductDisplayWithFilterParametersCooperative() {
         open(URL_STEAM_GENERAL);
         getWebDriver().manage().window().maximize();
         Assert.assertEquals(url(), URL_STEAM_GENERAL, "Opening the wrong site page");
-        MAIN_PAGE.clickStoreNavAreaPoint("Категории")
+
+        new SteamMainPage().clickStoreNavAreaPoint("Категории")
                 .clickSubparagraphLinkButtonOnCategory("Кооперативы");
-        COOP_PAGE.scrollFilterSectionFrame()
+
+        new CoopPage().scrollFilterSectionFrame()
                 .clickFilterGamesButton()
                 .clickSubparagraphOnPointBorderButtonOnFilter("Основные жанры", "Казуальная игра")
                 .collapseSectionOnFilter("Основные жанры")
                 .clickPointBorderButtonOnFilter("Игроки")
                 .clickSubparagraphOnPointBorderButtonOnFilter("Игроки", "Кооператив")
                 .clickCapsuleImageGameLink("Garry's Mod");
+
         ArrayList<String> tabs = new ArrayList<>(getWebDriver().getWindowHandles());
         switchTo().window(tabs.get(1));
-        Assert.assertEquals(GARRYS_MOD_PAGE.getTextAppName(), "Garry's Mod", "Произведен некорректный выбор игры");
+        GarrysModPage garrysModPage = new GarrysModPage();
+        Assert.assertEquals(garrysModPage.getTextAppName(), "Garry's Mod", "Произведен некорректный выбор игры");
     }
 }
